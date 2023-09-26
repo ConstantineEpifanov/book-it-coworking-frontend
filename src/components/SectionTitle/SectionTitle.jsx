752ab10ea7ab8af00ab2b0ceebe10ce0df1e407f
Vladislav Umarov <vladdevs@yandex.ru>
2023-09-26 16:04:50 +0300

    fix: отказ от модульного scss и корректировка title в storybook

--- src/components/SectionTitle/SectionTitle.jsx
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
-import styles from "./SectionTitle.module.scss";
+import "./SectionTitle.scss";
 
 export const SectionTitle = ({ titleText }) => (
-  <h2 className={styles.title}>{titleText}</h2>
+  <h2 className="section-title">{titleText}</h2>
 );
 SectionTitle.propTypes = {
   titleText: PropTypes.string,
