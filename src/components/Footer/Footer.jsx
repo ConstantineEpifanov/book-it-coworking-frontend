963e5e81819b54615df00d5023d5b3a1b7f3c543
Vladislav Umarov <vladdevs@yandex.ru>
2023-09-26 22:01:47 +0300

    feat: added footer / subscriptionForm

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./Footer.scss";
+import { Logo } from "../UI-kit/Logo/Logo";
+import InstagramIcon from "./icons/InstagramIcon";
+import TwitterIcon from "./icons/TwitterIcon";
+import YoutubeIcon from "./icons/YoutubeIcon";
+import TelegramIcon from "./icons/TelegramIcon";
+import { SubscriptionForm } from "../SubscriptionForm/SubscriptionForm";
+
+export const Footer = ({ onSubmit }) => (
+  <footer className="footer">
+    <div className="footer__container">
+      <div className="footer__info">
+        <div className="footer__column">
+          <Logo mode="green" width="70px" height="22px" />
+          <a href="/" className="footer__link">
+            Политика конфиденциальности
+          </a>
+        </div>
+        <div className="footer__column">
+          <a href="tel:+70000000000" className="footer__contacts">
+            +7(000)-00–00–00
+          </a>
+          <a href="mailto:info@itcoworking.com" className="footer__contacts">
+            info@itcoworking.com
+          </a>
+          <div className="footer__icons">
+            <InstagramIcon />
+            <TwitterIcon />
+            <YoutubeIcon />
+            <TelegramIcon />
+          </div>
+        </div>
+      </div>
+      <SubscriptionForm onSubmit={onSubmit} />
+    </div>
+  </footer>
+);
+
+Footer.propTypes = {
+  onSubmit: PropTypes.func,
+};
+
+Footer.defaultProps = {
+  onSubmit: () => {},
+};
---
+++ src/components/Footer/Footer.stories.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Footer } from "./Footer";
+
+export default {
+  title: "Components/Footer",
+  component: Footer,
+  tags: ["autodocs"],
+};
+
+export const FooterDemo = () => <Footer />;
