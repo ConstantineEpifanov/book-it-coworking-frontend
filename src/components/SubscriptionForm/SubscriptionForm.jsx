5ff87d609df24b6eb78620e04378c04a09ebd8a2
Vladislav Umarov <vladdevs@yandex.ru>
2023-09-28 08:53:26 +0300

    fix: updated form in footer

--- src/components/SubscriptionForm/SubscriptionForm.jsx
+++ src/components/SubscriptionForm/SubscriptionForm.jsx
@@ -4,27 +4,43 @@ import "./SubscriptionForm.scss";
 import Input from "../UI-kit/Input/Input";
 import Button from "../UI-kit/Button/Button";
 
-const SubscriptionForm = ({ onSubmit }) => (
-  <div className="subscription">
-    <p className="subscription__message">
-      Ненавязчивая рассылка об акциях и новостях сайта
-    </p>
-    <form className="subscription-form" onSubmit={onSubmit} noValidate>
-      <fieldset className="subscription-form__box">
-        <Input
-          inputType="email"
-          inputClass="subscription__input"
-          inputPlaceholder="E-mail"
-        />
-        <Button
-          btnClass="subscription__button"
-          btnText="Подписаться"
-          btnType="submit"
-        />
-      </fieldset>
-    </form>
-  </div>
-);
+const SubscriptionForm = ({ onSubmit }) => {
+  const onChangeInput = () => {};
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (onSubmit) {
+      onSubmit();
+    }
+  };
+  return (
+    <div className="subscription">
+      <p className="subscription__message">
+        Ненавязчивая рассылка об акциях и новостях сайта
+      </p>
+      <form className="subscription-form" onSubmit={handleSubmit} noValidate>
+        <fieldset className="subscription-form__box">
+          <div className="input__wrapper">
+            <Input
+              inputType="email"
+              inputClass="input__subscription"
+              inputPlaceholder="E-mail"
+              inputValue=""
+              onChangeInput={onChangeInput}
+            />
+          </div>
+          <Button
+            btnClass="button__subscription button_type_form"
+            btnText="Подписаться"
+            btnType="submit"
+            onClick=""
+            isValidBtn
+          />
+        </fieldset>
+      </form>
+    </div>
+  );
+};
 
 SubscriptionForm.propTypes = {
   onSubmit: PropTypes.func,
