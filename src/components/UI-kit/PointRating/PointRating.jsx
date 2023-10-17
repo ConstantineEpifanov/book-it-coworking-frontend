43bbc5fb855ecbcd53684435235007cf8b2d1d29
Constantine <asbest911@gmail.com>
2023-10-17 19:15:25 +0300

    fix: fix styles on Points

--- src/components/UI-kit/PointRating/PointRating.jsx
+++ src/components/UI-kit/PointRating/PointRating.jsx
@@ -14,7 +14,7 @@ export const PointRating = ({ rating, optionalClass }) => (
     >
       <path
         d="M12 2L15.09 8.26L22 9.27L17 14.14L18.18 21.02L12 17.77L5.82 21.02L7 14.14L2 9.27L8.91 8.26L12 2Z"
-        stroke="#b0ff1a"
+        stroke="black"
         strokeWidth="2"
         strokeLinecap="round"
         strokeLinejoin="round"
