807de4975bd78263d2ebf01220edd3f2e8d4acd6
Sergey Berdnikov <sega12b@yandex.ru>
2023-11-01 19:58:43 +0500

    fix: remove routes changes

--- src/App.jsx
+++ src/App.jsx
@@ -158,16 +158,7 @@ function App() {
             <Route path="/payments" element={<Payments />} />
             <Route path="*" element={<PageNotFound />} />
             {/* для рероутинга попапов, чтобы при переключении не бил в 404 */}
-            <Route
-              path="/popup/*"
-              element={
-                previousLocation?.pathname === "/booking" ? (
-                  <Booking />
-                ) : (
-                  <Main />
-                )
-              }
-            />
+            <Route path="/popup/*" element={<Main />} />
           </Routes>
           {previousLocation && (
             <Routes>
